61c4229ec16235c36598958b190b1d222dcdd41a
julianjay <julianjay12@gmail.com>
2015-06-27 12:30:24 -0400

    superclass for all pieces

---
+++ Piece.java
@@ -0,0 +1,37 @@
+public class Piece{
+
+    //variables
+    private final String color; //black (0) or red (1)
+    private boolean alive; //whether or not a piece has been captured
+ 
+    //constructors
+    public Piece(String s, boolean a){//takes color as a parameter
+	color = s;
+	alive = true;
+    }
+
+    //action methods
+
+    public void kill(Piece p){
+	p.setAlive(false);
+    }
+
+    //check methods
+    /*public void checkMoves(){
+
+      }*/
+
+    //set methods
+    public void setAlive(boolean a){
+	alive = a;
+    }
+
+    //get methods
+    public String getColor(){
+	return color;
+    }
+    public boolean isInPlay(){
+	return alive;
+    }
+
+}
